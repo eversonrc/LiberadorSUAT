7eda076ede1a9435edd41b73b3934c120a2c1535
mirndlazaro <mirandaalazaro@gmail.com>
2022-03-17 08:55:57 -0300

    feat: Mudança no design do front utilizando abas e modais

---
+++ LiberadorSUAT/Screens/Modals/ModalAnexos.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace LiberadorSUAT.Screens.Modals
+{
+    partial class ModalAnexos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModalAnexos));
+            this.listBoxAttachments = new System.Windows.Forms.ListBox();
+            this.btnExcluirDocumentacao = new System.Windows.Forms.Button();
+            this.btnAdcionarDocumentacao = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnExcluirArquivos = new System.Windows.Forms.Button();
+            this.btnAdicionarArquivos = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxArquivos = new System.Windows.Forms.ListBox();
+            this.listBoxScripts = new System.Windows.Forms.ListBox();
+            this.btnExcluirScript = new System.Windows.Forms.Button();
+            this.btnAdicionarScript = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnAjudaArquivos = new System.Windows.Forms.Button();
+            this.btnAjudaScripts = new System.Windows.Forms.Button();
+            this.btnAjudaDocs = new System.Windows.Forms.Button();
+            this.toolTipModalAnexo = new System.Windows.Forms.ToolTip(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // listBoxAttachments
+            // 
+            this.listBoxAttachments.ForeColor = System.Drawing.Color.Black;
+            this.listBoxAttachments.FormattingEnabled = true;
+            this.listBoxAttachments.Location = new System.Drawing.Point(425, 189);
+            this.listBoxAttachments.Name = "listBoxAttachments";
+            this.listBoxAttachments.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxAttachments.Size = new System.Drawing.Size(350, 82);
+            this.listBoxAttachments.TabIndex = 35;
+            // 
+            // btnExcluirDocumentacao
+            // 
+            this.btnExcluirDocumentacao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnExcluirDocumentacao.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluirDocumentacao.Location = new System.Drawing.Point(682, 280);
+            this.btnExcluirDocumentacao.Name = "btnExcluirDocumentacao";
+            this.btnExcluirDocumentacao.Size = new System.Drawing.Size(93, 26);
+            this.btnExcluirDocumentacao.TabIndex = 34;
+            this.btnExcluirDocumentacao.Text = "Excluir";
+            this.btnExcluirDocumentacao.UseVisualStyleBackColor = false;
+            this.btnExcluirDocumentacao.Click += new System.EventHandler(this.btnExcluirDocumentacao_Click);
+            // 
+            // btnAdcionarDocumentacao
+            // 
+            this.btnAdcionarDocumentacao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnAdcionarDocumentacao.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdcionarDocumentacao.Location = new System.Drawing.Point(425, 280);
+            this.btnAdcionarDocumentacao.Name = "btnAdcionarDocumentacao";
+            this.btnAdcionarDocumentacao.Size = new System.Drawing.Size(93, 26);
+            this.btnAdcionarDocumentacao.TabIndex = 33;
+            this.btnAdcionarDocumentacao.Text = "Adicionar";
+            this.btnAdcionarDocumentacao.UseVisualStyleBackColor = false;
+            this.btnAdcionarDocumentacao.Click += new System.EventHandler(this.btnAdcionarDocumentacao_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(422, 168);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(168, 16);
+            this.label10.TabIndex = 32;
+            this.label10.Text = "Documentações Extras";
+            // 
+            // btnExcluirArquivos
+            // 
+            this.btnExcluirArquivos.BackColor = System.Drawing.Color.White;
+            this.btnExcluirArquivos.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluirArquivos.Image = ((System.Drawing.Image)(resources.GetObject("btnExcluirArquivos.Image")));
+            this.btnExcluirArquivos.Location = new System.Drawing.Point(742, 122);
+            this.btnExcluirArquivos.Name = "btnExcluirArquivos";
+            this.btnExcluirArquivos.Size = new System.Drawing.Size(33, 41);
+            this.btnExcluirArquivos.TabIndex = 40;
+            this.btnExcluirArquivos.UseVisualStyleBackColor = false;
+            this.btnExcluirArquivos.Click += new System.EventHandler(this.btnExcluirArquivos_Click);
+            // 
+            // btnAdicionarArquivos
+            // 
+            this.btnAdicionarArquivos.BackColor = System.Drawing.Color.White;
+            this.btnAdicionarArquivos.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionarArquivos.ForeColor = System.Drawing.Color.White;
+            this.btnAdicionarArquivos.Image = ((System.Drawing.Image)(resources.GetObject("btnAdicionarArquivos.Image")));
+            this.btnAdicionarArquivos.Location = new System.Drawing.Point(742, 73);
+            this.btnAdicionarArquivos.Name = "btnAdicionarArquivos";
+            this.btnAdicionarArquivos.Size = new System.Drawing.Size(33, 43);
+            this.btnAdicionarArquivos.TabIndex = 39;
+            this.btnAdicionarArquivos.UseVisualStyleBackColor = false;
+            this.btnAdicionarArquivos.Click += new System.EventHandler(this.btnAdicionarArquivos_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(20, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "Arquivos";
+            // 
+            // listBoxArquivos
+            // 
+            this.listBoxArquivos.ForeColor = System.Drawing.Color.Black;
+            this.listBoxArquivos.FormattingEnabled = true;
+            this.listBoxArquivos.Location = new System.Drawing.Point(23, 73);
+            this.listBoxArquivos.Name = "listBoxArquivos";
+            this.listBoxArquivos.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxArquivos.Size = new System.Drawing.Size(713, 82);
+            this.listBoxArquivos.TabIndex = 37;
+            // 
+            // listBoxScripts
+            // 
+            this.listBoxScripts.ForeColor = System.Drawing.Color.Black;
+            this.listBoxScripts.FormattingEnabled = true;
+            this.listBoxScripts.Location = new System.Drawing.Point(26, 189);
+            this.listBoxScripts.Name = "listBoxScripts";
+            this.listBoxScripts.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxScripts.Size = new System.Drawing.Size(348, 82);
+            this.listBoxScripts.TabIndex = 44;
+            // 
+            // btnExcluirScript
+            // 
+            this.btnExcluirScript.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnExcluirScript.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluirScript.Location = new System.Drawing.Point(278, 280);
+            this.btnExcluirScript.Name = "btnExcluirScript";
+            this.btnExcluirScript.Size = new System.Drawing.Size(96, 26);
+            this.btnExcluirScript.TabIndex = 43;
+            this.btnExcluirScript.Text = "Excluir";
+            this.btnExcluirScript.UseVisualStyleBackColor = false;
+            this.btnExcluirScript.Click += new System.EventHandler(this.btnExcluirScript_Click);
+            // 
+            // btnAdicionarScript
+            // 
+            this.btnAdicionarScript.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnAdicionarScript.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionarScript.Location = new System.Drawing.Point(26, 280);
+            this.btnAdicionarScript.Name = "btnAdicionarScript";
+            this.btnAdicionarScript.Size = new System.Drawing.Size(96, 26);
+            this.btnAdicionarScript.TabIndex = 42;
+            this.btnAdicionarScript.Text = "Adicionar";
+            this.btnAdicionarScript.UseVisualStyleBackColor = false;
+            this.btnAdicionarScript.Click += new System.EventHandler(this.btnAdicionarScript_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(23, 168);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(208, 16);
+            this.label8.TabIndex = 41;
+            this.label8.Text = "Scripts de Banco de Dados";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(682, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 26);
+            this.button1.TabIndex = 45;
+            this.button1.Text = "Concluir";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button2.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(23, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 26);
+            this.button2.TabIndex = 46;
+            this.button2.Text = "Voltar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnAjudaArquivos
+            // 
+            this.btnAjudaArquivos.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAjudaArquivos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnAjudaArquivos.Cursor = System.Windows.Forms.Cursors.Help;
+            this.btnAjudaArquivos.Image = ((System.Drawing.Image)(resources.GetObject("btnAjudaArquivos.Image")));
+            this.btnAjudaArquivos.Location = new System.Drawing.Point(92, 44);
+            this.btnAjudaArquivos.Name = "btnAjudaArquivos";
+            this.btnAjudaArquivos.Size = new System.Drawing.Size(24, 26);
+            this.btnAjudaArquivos.TabIndex = 47;
+            this.btnAjudaArquivos.UseVisualStyleBackColor = false;
+            // 
+            // btnAjudaScripts
+            // 
+            this.btnAjudaScripts.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAjudaScripts.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnAjudaScripts.Cursor = System.Windows.Forms.Cursors.Help;
+            this.btnAjudaScripts.Image = ((System.Drawing.Image)(resources.GetObject("btnAjudaScripts.Image")));
+            this.btnAjudaScripts.Location = new System.Drawing.Point(237, 162);
+            this.btnAjudaScripts.Name = "btnAjudaScripts";
+            this.btnAjudaScripts.Size = new System.Drawing.Size(24, 26);
+            this.btnAjudaScripts.TabIndex = 48;
+            this.btnAjudaScripts.UseVisualStyleBackColor = false;
+            // 
+            // btnAjudaDocs
+            // 
+            this.btnAjudaDocs.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAjudaDocs.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnAjudaDocs.Cursor = System.Windows.Forms.Cursors.Help;
+            this.btnAjudaDocs.Image = ((System.Drawing.Image)(resources.GetObject("btnAjudaDocs.Image")));
+            this.btnAjudaDocs.Location = new System.Drawing.Point(596, 161);
+            this.btnAjudaDocs.Name = "btnAjudaDocs";
+            this.btnAjudaDocs.Size = new System.Drawing.Size(24, 26);
+            this.btnAjudaDocs.TabIndex = 49;
+            this.btnAjudaDocs.UseVisualStyleBackColor = false;
+            // 
+            // ModalAnexos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 438);
+            this.Controls.Add(this.btnAjudaDocs);
+            this.Controls.Add(this.btnAjudaScripts);
+            this.Controls.Add(this.btnAjudaArquivos);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBoxScripts);
+            this.Controls.Add(this.btnExcluirScript);
+            this.Controls.Add(this.btnAdicionarScript);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnExcluirArquivos);
+            this.Controls.Add(this.btnAdicionarArquivos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxArquivos);
+            this.Controls.Add(this.listBoxAttachments);
+            this.Controls.Add(this.btnExcluirDocumentacao);
+            this.Controls.Add(this.btnAdcionarDocumentacao);
+            this.Controls.Add(this.label10);
+            this.Name = "ModalAnexos";
+            this.Text = "ModalAnexos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxAttachments;
+        private System.Windows.Forms.Button btnExcluirDocumentacao;
+        private System.Windows.Forms.Button btnAdcionarDocumentacao;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnExcluirArquivos;
+        private System.Windows.Forms.Button btnAdicionarArquivos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxArquivos;
+        private System.Windows.Forms.ListBox listBoxScripts;
+        private System.Windows.Forms.Button btnExcluirScript;
+        private System.Windows.Forms.Button btnAdicionarScript;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnAjudaArquivos;
+        private System.Windows.Forms.Button btnAjudaScripts;
+        private System.Windows.Forms.Button btnAjudaDocs;
+        private System.Windows.Forms.ToolTip toolTipModalAnexo;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
---
+++ LiberadorSUAT/Screens/Modals/ModalAnexos.cs
@@ -0,0 +1,115 @@
+﻿using LiberadorSUAT.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiberadorSUAT.Screens.Modals
+{
+    public partial class ModalAnexos : Form
+    {
+        public ModalAnexos()
+        {
+            InitializeComponent();
+            ConfigurarToolTip();
+        }
+        private void ConfigurarToolTip()
+        {
+            toolTipModalAnexo.AutoPopDelay = 4000;
+            toolTipModalAnexo.InitialDelay = 100;
+            toolTipModalAnexo.ReshowDelay = 200;
+
+            toolTipModalAnexo.ToolTipTitle = "Dica";
+            toolTipModalAnexo.IsBalloon = true;
+            toolTipModalAnexo.ToolTipIcon = ToolTipIcon.Info;
+
+            toolTipModalAnexo.SetToolTip(btnAjudaArquivos, "Insira as alterações realizadas no sistema de acordo com o helpdesk informado.");
+            toolTipModalAnexo.SetToolTip(btnAjudaScripts, "Insira as alterações realizadas no sistema de acordo com o helpdesk informado.");
+            toolTipModalAnexo.SetToolTip(btnAjudaDocs, "Insira as alterações realizadas no sistema de acordo com o helpdesk informado.");
+        }
+
+        // ANEXAR Arquivos de compilação
+        //
+        private void btnAdicionarArquivos_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string[] path = Directory.GetFileSystemEntries(folderBrowserDialog1.SelectedPath);
+                string direc = String.Join("", path);
+                DirectoryInfo dInfo = new DirectoryInfo(direc);
+
+                string fullName = dInfo.FullName;
+
+                listBoxArquivos.Items.Add(fullName);
+
+                foreach(FileInfo file in dInfo.GetFiles())
+                {
+                    listBoxArquivos.Items.Add(file.FullName);
+                }
+            }
+            /*
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+            dialog.Multiselect = true;
+            //dialog.Filter = "Xml Files|(*.xml)|*.xml";
+            dialog.Filter = "Word Documents|*.doc|PDF Files|*.pdf";
+
+            foreach (var file in dialog.FileNames)
+            {
+                listBoxArquivos.Items.Add(file);
+            }
+            }
+
+            listBoxArquivos.IntegralHeight = true;
+            // Display a horizontal scroll bar.
+            listBoxArquivos.HorizontalScrollbar = true;
+            */
+        }
+        
+        private void btnExcluirArquivos_Click(object sender, EventArgs e)
+        {
+            Arquivo arquivo = new Arquivo();
+            arquivo.ExcluirArquivos(listBoxArquivos);
+        }
+
+        // ANEXAR Documentação
+        //
+        private void btnAdcionarDocumentacao_Click(object sender, EventArgs e)
+        {
+            Arquivo arquivo = new Arquivo();
+            arquivo.AdicionarArquivos(listBoxAttachments, 0);
+        }
+        private void btnExcluirDocumentacao_Click(object sender, EventArgs e)
+        {
+            Arquivo arquivo = new Arquivo();
+            arquivo.ExcluirArquivos(listBoxAttachments);
+        }
+
+        
+        // ANEXAR Scripts de Banco de Dados
+        //
+        private void btnExcluirScript_Click(object sender, EventArgs e)
+        {
+            Arquivo arquivo = new Arquivo();
+            arquivo.ExcluirArquivos(listBoxScripts);
+        }
+
+        private void btnAdicionarScript_Click(object sender, EventArgs e)
+        {
+            Arquivo arquivo = new Arquivo();
+            arquivo.AdicionarArquivos(listBoxScripts, 2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
